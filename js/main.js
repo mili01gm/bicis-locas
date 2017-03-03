ec6e399ed675bbd0dd3c3665b064f3f184771ff5
Luz Gutierrez <mili01gm@gmail.com>
2017-03-03 11:14:17 -0500

    cambios en clase

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+  function validar(param){
+    if(param != undefined) { break;}
+    else { continue;}
+  }
+
+  function nomAp(param){
+    if(param[])
+  }
+
+
 }
