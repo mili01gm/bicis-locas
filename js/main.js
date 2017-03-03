f1b66283ccb6f929023e77738ab113f98602ef2c
Luz Gutierrez <mili01gm@gmail.com>
2017-03-03 12:53:21 -0500

    Ejercicio solucionado

--- js/main.js
+++ js/main.js
@@ -1,38 +1,51 @@
 function validateForm(){
 	/* Escribe tú código aquí */
-/*  function validar(param){
-    if(param != undefined) { break;}
-    else { continue;}
-  }
-
-  function nomAp(param){
-    if(param[])
-  }
-*/
   var nombre = document.getElementById('name');
   var apellido = document.getElementById('lastname');
   var email = document.getElementById('input-email');
   var password = document.getElementById('input-password');
-  var bici = document.getElementById('selecciona');
+  var selecciona = document.getElementById('selecciona');
+  var bici = selecciona.options[selecciona.selectedIndex].value;
   var twitter = document.getElementById('input-social');
-  var button = document.getElementsById('boton');
 
-  function obligatorio(param){
-      is(param == undefined){
-        return "<span>"+"Campo obligatorio"+"</span>";
-      } else { continue;}
-  }
+// Para validaciones de letras y cosas del nombre...
+  var soloLetras = /^[a-zñÑA-Z]*$/;
+  var mayus = /^[A-Z]{1}/;
 
-  function nameLast(param){
+// Descativo funciones y dejo condicionales
+// porque no corren con las funciones!!!!! T_T
 
+// Estos son los obligatorios. Sin buscar el botón.
+    //function obligados(){
+      if(nombre.value.length == 0){
+        alert("Ingrese un nombre");}
+      else if(apellido.value.length == 0){
+        alert("Ingrese un apellido");}
+      else if(email.value.length == 0){
+        alert("Ingrese un correo válido");}
+      else if(password.value.length < 6){
+        alert("Ingrese una contraseña válida");}
+      else if(bici == "0"){
+        alert("Seleccione un tipo de bicicleta");}
+    //}
+    //obligados();
 
-  }
+    //function adicionales(){
+      if(nombre.value.search(soloLetras) || apellido.value.search(soloLetras)){
+        alert("Ingrese solo letras en Nombre y/o Apellido");}
+      else if(nombre.value.search(mayus)){
+        alert("La primera letra de tu nombre debe ser mayúscula");}
+      else if(apellido.value.search(mayus)){
+        alert("La primera letra de tu apellido debe ser mayúscula");}
+      else if(password.value == "password" || password.value == "123456" || password.value =="987654"){
+        alert("La contaseña no puede ser "+ password.value);
+      }
+    //}
+    //adicionales();
 
-  button.addEventListener('click', function(){
-    email = validateForm(email.value);
-    password = validateForm(password.value);
-    bici = validateForm(bici.value);
-    twitter = validateForm(twitter.value);
-  })
+    // Con esto ya no valida el resto...
+    /*document.getElementById('envia').addEventListener('click',function(){
+      alert("Gracias por participar");
+    });*/
 
 }
